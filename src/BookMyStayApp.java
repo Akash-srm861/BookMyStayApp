287e504762b8172ca7d4a1d0653cf6d229e67a03
joshuaabrahamthomas19-lang <joshuaabrahamthomas19@gmail.com>
2026-03-10 14:18:53 +0530

    Add welcome message module

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,18 @@
+/**
+ * =============================================================================
+ * MAIN CLASS - UseCase1HotelBookingApp
+ * =============================================================================
+ * * Use Case 1: Application Entry & Welcome Message
+ * @version 1.0
+ */
+public class BookMyStayApp{
+
+    /**
+     * Application entry point.
+     * * This method is the first method executed
+     */
+    public static void main(String[] args) {
+        // Displaying the welcome message
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }}
