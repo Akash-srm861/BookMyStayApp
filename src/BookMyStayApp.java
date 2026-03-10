4d9e38ff1b049d3f70310d719f016fe6bb8cd687
joshuaabrahamthomas19-lang <joshuaabrahamthomas19@gmail.com>
2026-03-10 14:39:35 +0530

    UC2: Implement Room hierarchy in BookMyStayApp.java

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,18 +1,63 @@
 /**
  * =============================================================================
- * MAIN CLASS - UseCase1HotelBookingApp
+ * ABSTRACT CLASS - Room
  * =============================================================================
- * * Use Case 1: Application Entry & Welcome Message
- * @version 1.0
  */
-public class BookMyStayApp{
+abstract class Room {
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
 
-    /**
-     * Application entry point.
-     * * This method is the first method executed
-     */
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds + " | Size: " + squareFeet + " sqft | Price: " + pricePerNight);
+    }
+}
+
+/**
+ * =============================================================================
+ * CHILD CLASSES - SingleRoom and DoubleRoom
+ * =============================================================================
+ */
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+/**
+ * =============================================================================
+ * MAIN CLASS - BookMyStayApp
+ * =============================================================================
+ * Use Case 2: Basic Room Types & Static Availability
+ * @version 2.1
+ */
+public class BookMyStayApp {
+
     public static void main(String[] args) {
-        // Displaying the welcome message
         System.out.println("Welcome to the Hotel Booking Management System");
         System.out.println("System initialized successfully.");
-    }}
+        System.out.println("----------------------------------------------");
+
+        // Initializing room types using Polymorphism
+        Room single = new SingleRoom();
+        Room dual = new DoubleRoom();
+
+        System.out.print("Single Room Details: ");
+        single.displayRoomDetails();
+
+        System.out.print("Double Room Details: ");
+        dual.displayRoomDetails();
+    }
+}
